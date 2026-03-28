0421df60e8fe966ec832ffaffb08eb9841408201
Andrius Prieskienis <andrius@prieskienis.eu>
2026-03-28 07:34:07 -0400

    Logi n screen

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,17 +1,63 @@
 
 import React, { useState } from 'react';
 import './LoginPage.css';
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
 
     //insert code here to create useState hook variables for email, password
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [incorrect, setIncorrect] = useState('');
 
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+          navigate('/app')
+        }
+      }, [navigate])
+    
     // insert code here to create handleLogin function and include console.log
-    const handleLogin = async () => {
-        console.log("Inside handleLogin");
-	}
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            })
+        });
+
+        const json = await res.json();
+        console.log('Json',json);
+        if (json.authtoken) {
+
+          sessionStorage.setItem('auth-token', json.authtoken);
+          sessionStorage.setItem('name', json.userName);
+          sessionStorage.setItem('email', json.userEmail);
+
+          setIsLoggedIn(true);
+
+          navigate('/app');
+        } else {
+
+          document.getElementById("email").value="";
+          document.getElementById("password").value="";
+          setIncorrect("Wrong password. Try again.");
+          setTimeout(() => {
+            setIncorrect("");
+          }, 2000);
+        }
+      }
 
     return (
       <div className="container mt-5">
@@ -46,7 +92,7 @@ function LoginPage() {
                     />
                 </div>
 
-  		{/* insert code here to create a button that performs the `handleLogin` function on click */}
+                <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
                 <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
 
                 <p className="mt-4 text-center">
